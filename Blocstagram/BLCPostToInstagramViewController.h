7193e08f7c78fab29aa0e42809aba4c5116f6d67
David Krasnove <krasnove@mac.com>
2015-02-01 11:08:28 -0800

    Photo Filters and Sending to Instagram

---
+++ Blocstagram/BLCPostToInstagramViewController.h
@@ -0,0 +1,18 @@
+//
+//  BLCPostToInstagramViewController.h
+//  Blocstagram
+//
+//  Created by dbk-dev on 1/30/15.
+//  Copyright (c) 2015 Bloc. All rights reserved.
+//
+
+
+#import <UIKit/UIKit.h>
+
+@interface BLCPostToInstagramViewController : UIViewController
+
+- (instancetype) initWithImage:(UIImage *)sourceImage;
+
+
+
+@end
